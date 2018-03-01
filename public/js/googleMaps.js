31c5675dc9ce2feccbffdd64715d63c88731e242
v1nnyc <cannallav019@yahoo.com>
2018-03-01 09:59:45 -0800

    finished the entire thing omg

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,23 @@
 function initMap() {
 	// add your code here
+	
+		// Create center marker at UCSD
+		var ucsd_ltlng = {lat:32.880, lng:-117.236};
+		//var cicc_ltlng ={lat:32.884985,lng:-117.241296};
+		var ssc_ltlng = {lat: 32.878819, lng: -117.235913};
+	
+		// Create a map object and specify the DOM element for display.
+		var map = new google.maps.Map(document.getElementById('map'), {
+		  center: ssc_ltlng,
+		  zoom: 15
+		});
+	  
+		
+
+		var marker = new google.maps.Marker({
+			position: ssc_ltlng,
+			map: map,
+			title: 'UCSD'
+		});
+	
 }
